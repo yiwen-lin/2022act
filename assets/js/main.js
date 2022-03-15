8df083bccec79a50228d2c6624e008a384cf9119
YiWen Lin <yiwenlin@cw.com.tw>
2022-03-15 18:49:13 +0800

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,50 +24,18 @@
     }
   });
 
-  // side Nav & background color
-  // var lastId,
-  //     seasonsY=$('#seasons').offset().top,
-  //     headerH=$('.header').outerHeight(true),
-  //     menuItems = $('#sideNav').find("a"),
-  //     scrollItems = menuItems.map(function(){
-  //       var item = $($(this).attr("href"));
-  //       if (item.length) { return item; }
-  //     });
-  // var winH=$(window).outerHeight(true)
+//click none
+  $(document).ready(function(){
+    $("#hide").click(function(){
+      $(".fix-right").hide();
+    });
+  });
 
-  //   menuItems.on('click',function(e){
-  //     e.preventDefault();
-  //     var seasonName=$(this).attr('href');
-  //     var $target=$(seasonName);
-  //     $('html,body').animate({ scrollTop: $target.offset().top + 1 }, 800);
-  //     return false;
-  //   });
 
-  //   $(window).on('scroll',function(){
-  //     var scrollDistance = $(this).scrollTop()+headerH,
-  //         seasonsY=$('#seasons').offset().top,
-  //         cerebrityY=$('#cerebrity').offset().top;
-  //     if (scrollDistance > (seasonsY-200)&& (cerebrityY-scrollDistance)>winH-100) {
-  //       $('#sideNav').addClass('active');
-  //     } else {
-  //       $('#sideNav').removeClass('active');
-  //     }
-  //     var cur = scrollItems.map(function(){
-  //       if ($(this).offset().top <= scrollDistance + 60){
-  //           return this;
-  //       }
-  //     });
-  //     // Get the id of the current element
-  //     cur = cur[cur.length-1];
-  //     var id = cur && cur.length ? cur[0].id : "";
-  //         if (lastId !== id) {
-  //         lastId = id;
-  //             $('#seasons').removeClass(function (index, css) {
-  //             return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-  //         });
-  //             $('#seasons').addClass('color-' + id);
-  //         menuItems
-  //             .parent().removeClass("active")
-  //             .end().filter("[href=#"+id+"]").parent().addClass("active");
-  //       }
-  //   });
+
+  $(function(){
+    var elem= document.getElementById("exam");
+    $("#fix-right").css("display","block");
+  })
+
+  
